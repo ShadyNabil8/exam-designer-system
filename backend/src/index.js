c803121d4683eaf28a4eb6694302397a3707cb4b
shady <shadyngheith@gmail.com>
2024-10-14 15:26:17 +0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./database");
+const {
+  errorHandlerMiddleware,
+} = require("./middlewares/errorHandlerMiddleware");
+const logger = require("morgan");
+require("dotenv").config();
+const { swaggerUi, swaggerSpec } = require("./swagger");
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(errorHandlerMiddleware);
+
+if (process.env.NODE_ENV !== "test") {
+  dbConnect();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(
+      `Swagger docs available at http://localhost:${process.env.PORT}/api-docs`
+    );
+  });
+}
+
+module.exports = app;
---
+++ backend/src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,21 @@
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const errorHandlerMiddleware = function (err, req, res, next) {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  if (process.env.NODE_ENV !== "test") {
+    console.error(`Error: ${message}, Status Code: ${statusCode}`);
+  }
+
+  res.status(statusCode).json({
+    message,
+  });
+};
+
+module.exports = { errorHandlerMiddleware, CustomError };
