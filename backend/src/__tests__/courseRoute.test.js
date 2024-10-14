c741ba84dfd4a4a7974a29a52d7f5fc79efb144c
shady <shadyngheith@gmail.com>
2024-10-14 19:06:09 +0300

    Add POST /api/courses tests for error handlin and 200 status code

--- backend/src/__tests__/courseRoute.test.js
+++ backend/src/__tests__/courseRoute.test.js
@@ -1,5 +1,8 @@
 const request = require("supertest");
 const app = require("../index");
+const database = require("../database");
+
+jest.mock("../database");
 
 describe("POST /courses", () => {
   beforeEach(() => {
@@ -30,5 +33,41 @@ describe("POST /courses", () => {
     expect(response.body.errors[0].msg).toEqual(
       "Number of Chapters is required and must be a valid positive integer!"
     );
+  });
+
+  it("should return 200 course is successfully added", async () => {
+    // Mocking the addedCourse object
+    const mockCourse = {
+      id: "mockId",
+      name: "CS50",
+      numberOfChapters: 5,
+    };
+
+    database.addCourse.mockResolvedValue(mockCourse);
+
+    const response = await request(app).post("/api/courses").send({
+      name: "CS50",
+      numberOfChapters: 5,
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toEqual("The course was successfully added.");
+
+    expect(response.body).toHaveProperty("addedCourse");
+    expect(response.body.addedCourse).toEqual(mockCourse);
+  });
+
+  it("should handle errors and call next with an error", async () => {
+    // Simulate an error in `database.addCourse`.
+    const mockError = new Error("Database error");
+    database.addCourse.mockRejectedValue(mockError);
+
+    const response = await request(app).post("/api/courses").send({
+      name: "CS50",
+      numberOfChapters: 5,
+    });
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body.message).toBe("Database error");
   });
 });
