90d70d209a402a0e8009f5db4dcbade1e0434a0f
shady <shadyngheith@gmail.com>
2024-10-18 14:15:17 +0300

    Implement CoursePage

--- frontend/src/pages/CoursePage.jsx
+++ frontend/src/pages/CoursePage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetchData from "../hooks/useFetchData";
+import api from "../api/api";
 
 const CoursePage = () => {
-  return (
-    <div>CoursePage</div>
-  )
-}
+  const { courseId } = useParams();
 
-export default CoursePage
+  const { data: course, isLoading } = useFetchData(
+    ["courses", courseId],
+    async () => {
+      try {
+        const response = await api.get(`/api/courses/${courseId}`);
+        return response.data.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  );
+
+  if (isLoading) {
+    return <p>Loading courses...</p>;
+  }
+
+  return (
+    <div className="flex flex-col gap-5">
+      <p className="p-4 text-3xl">Course Details</p>
+      <div>
+        <div className="flex gap-2">
+          <span className="font-bold">Name:</span>
+          <span className="">{course.name}</span>
+        </div>
+        <div className="flex gap-2">
+          <span className="font-bold">Number of chapters:</span>
+          <span className="">{course.numberOfChapters}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoursePage;
