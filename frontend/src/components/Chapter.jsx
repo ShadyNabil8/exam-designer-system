7da26aa9aead48b995c030cfbcb189fd495706ad
shady <shadyngheith@gmail.com>
2024-10-18 15:21:59 +0300

    Add Chapters page and chapter component

---
+++ frontend/src/components/Chapter.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import FloatingDeleteBtn from "../components/FloatingDeleteBtn";
+
+const Chapter = ({
+  chapterName,
+  chapterNumber,
+  maxNumberOfQuestions,
+  courseName,
+  chapterId,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Link
+      to={`/chapters/${chapterId}`}
+      className="relative w-full rounded-md border border-gray-400 p-4 transition-colors hover:bg-gray-50"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="grid grid-cols-4 gap-4">
+        <div className="font-bold">Name</div>
+        <div className="font-bold">Number</div>
+        <div className="font-bold">Course Name</div>
+        <div className="font-bold">Maximum number of questions</div>
+        <div>{chapterName}</div>
+        <div>{chapterNumber}</div>
+        <div>{courseName}</div>
+        <div>{maxNumberOfQuestions}</div>
+      </div>
+      {isHovered && (
+        <div className="vertical-center absolute right-4">
+          <FloatingDeleteBtn
+            deleteUrl={`api/chapters/${chapterId}`}
+            queryKey={["chapters"]}
+          />
+        </div>
+      )}
+    </Link>
+  );
+};
+
+export default Chapter;
--- frontend/src/pages/ChaptersPage.jsx
+++ frontend/src/pages/ChaptersPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Chapter from "../components/Chapter";
+import api from "../api/api";
+import useNotifier from "../hooks/useNotifier";
+import useFetchData from "../hooks/useFetchData";
 
 const ChaptersPage = () => {
-  return (
-    <div>ChaptersPage</div>
-  )
-}
+  const notify = useNotifier();
+  const { data: chapters, isLoading } = useFetchData(["chapters"], async () => {
+    try {
+      const response = await api.get("/api/chapters");      
+      return response.data.data;
+    } catch (error) {
+      notify.error(error.response?.data?.message || "Something went wrong!");
+    }
+  });
 
-export default ChaptersPage
+  if (isLoading) {
+    return <p>Loading courses...</p>;
+  }
+  return (
+    <div className="flex w-full flex-col gap-2 p-4">
+      {chapters.map((chapter, index) => (
+        <Chapter
+          key={index}
+          chapterName={chapter.name}
+          chapterNumber={chapter.number}
+          // courseName={chapter.course.name}
+          // maxNumberOfQuestions={chapter.maxNumberOfQuestions}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ChaptersPage;
